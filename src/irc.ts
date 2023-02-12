bcbba32f92448c3f1409e29f69c7160ac5c1edba
Piotr Luczko <piotr.luczko@gmail.com>
2023-02-12 14:58:31 +0100

    eslint

--- src/irc.ts
+++ src/irc.ts
@@ -1,9 +1,9 @@
-// @ts-ignore
-import * as IRC from "irc-framework";
-import { IrcEvents } from "./types";
-import { sicServerSocket } from "./main";
+// @ts-expect-error missing ts declaration file
+import * as IRC from 'irc-framework'
+import { IrcEvents } from './types'
+import { sicServerSocket } from './main'
 
-export const ircClient = new IRC.Client();
+export const ircClient = new IRC.Client()
 
 // Once the client has connected and successfully registered on the IRC network.
 // This is a good place to start joining channels.
@@ -12,29 +12,29 @@ export const ircClient = new IRC.Client();
 //     nick: nick
 // }
 ircClient.on(IrcEvents.connected, (_event: unknown) => {
-  sicServerSocket.emit("sic-irc-event", { type: IrcEvents.connected });
-});
+  sicServerSocket.emit('sic-irc-event', { type: IrcEvents.connected })
+})
 
 // The client has disconnected from the network and failed to auto reconnect (if enabled).
 //
 // { }
 ircClient.on(IrcEvents.close, (_event: unknown) => {
-  sicServerSocket.emit("sic-irc-event", { type: IrcEvents.close });
-});
+  sicServerSocket.emit('sic-irc-event', { type: IrcEvents.close })
+})
 
 // The client has disconnected from the network.
 //
 // { }
 ircClient.on(IrcEvents.socketClose, (_event: unknown) => {
-  sicServerSocket.emit("sic-irc-event", { type: IrcEvents.socketClose });
-});
+  sicServerSocket.emit('sic-irc-event', { type: IrcEvents.socketClose })
+})
 
 // The client has a connected socket to the network. Network registration will automatically start at this point.
 //
 // { }
 ircClient.on(IrcEvents.socketConnected, (_event: unknown) => {
-  sicServerSocket.emit("sic-irc-event", { type: IrcEvents.socketConnected });
-});
+  sicServerSocket.emit('sic-irc-event', { type: IrcEvents.socketConnected })
+})
 
 // A valid raw line sent or received from the IRC server.
 //
@@ -43,10 +43,13 @@ ircClient.on(IrcEvents.socketConnected, (_event: unknown) => {
 //     from_server: true
 // }
 ircClient.on(IrcEvents.raw, (event: any) => {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (event?.from_server && event?.line) {
-    sicServerSocket.emit("sic-irc-event", { type: IrcEvents.raw, line: event.line });
+    sicServerSocket.emit('sic-irc-event', { type: IrcEvents.raw, line: event.line })
   }
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
   if (!event?.from_server && event?.line) {
-    console.log(`-> ${event.line}`);
+    // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+    console.log(`-> ${event.line}`)
   }
-});
+})
--- src/types.ts
+++ src/types.ts
@@ -1,46 +1,46 @@
 // https://github.com/kiwiirc/irc-framework/blob/master/docs/clientapi.md
 
 export enum IrcEvents {
-  connected = "connected",
-  close = "close",
-  socketClose = "socket close",
-  socketConnected = "socket connected",
-  raw = "raw",
+  connected = 'connected',
+  close = 'close',
+  socketClose = 'socket close',
+  socketConnected = 'socket connected',
+  raw = 'raw',
 }
 
 export enum IrcCommand {
-  connect = "connect",
-  disconnect = "disconnect",
-  raw = "raw",
+  connect = 'connect',
+  disconnect = 'disconnect',
+  raw = 'raw',
 }
 
-export type SICWebSocketPayload = {
-  type: IrcEvents | IrcCommand;
-  event?: unknown;
-};
+export interface SICWebSocketPayload {
+  type: IrcEvents | IrcCommand
+  event?: unknown
+}
 
-export type ConnectCommandPayload = {
-  type: string;
+export interface ConnectCommandPayload {
+  type: string
   event: {
-    nick: string;
+    nick: string
     server: {
-      host: string;
-      port: number;
-      encoding?: string;
-    };
-  };
-};
+      host: string
+      port: number
+      encoding?: string
+    }
+  }
+}
 
-export type DisconnectCommandPayload = {
-  type: string;
+export interface DisconnectCommandPayload {
+  type: string
   event?: {
-    quitReason?: string;
-  };
-};
+    quitReason?: string
+  }
+}
 
-export type RawCommandPayload = {
-  type: string;
+export interface RawCommandPayload {
+  type: string
   event: {
-    rawData: string[];
-  };
-};
+    rawData: string[]
+  }
+}
