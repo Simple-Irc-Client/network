9c66d57f2cdd2b416e020a77d6a6f7d998e5a0a7
Piotr Luczko <piotr.luczko@gmail.com>
2023-03-27 01:42:58 +0200

    default quit msg

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const port = 8667;
-export const defaultQuitMessage = 'Simple Irc Client ( https://simpleircclient.com )';
+export const defaultQuitMessage = 'Leaving'; // 'Simple Irc Client ( https://simpleircclient.com )';
 export const defaultIrcVersionMessage = 'Simple Irc Client';
 export const defaultIrcGecosMessage = 'Simple Irc Client user';
